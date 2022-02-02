7bf0c2646daa0ebdcd440cb0d259246e028288d8
Yao Yi <e0564217@u.nus.edu>
2022-02-02 17:42:29 +0800

    part 1 done

--- Lab 1/part1/queue.h
+++ Lab 1/part1/queue.h
@@ -2,7 +2,14 @@
 // Maximum number of elements in a queue
 //
 #define MAX_Q_SIZE	10
-
+void enq(double);
+double deq();
+void sum(double);
+void prod(double);
+void clear_sum();
+void clear_prod();
+double reduce();
+double flex_reduce(void (*)(void), void (*)(double));
 // Place function prototypes here.
 
 
